4132bb43a5f9607b81525a0df061d50bcd97e9aa
Jared Stroud <1250113+jaredestroud@users.noreply.github.com>
2019-11-09 21:44:25 -0500

    Merged separate functions into one. Actions are based on CLI args.

--- safeheader.py
+++ safeheader.py
@@ -4,30 +4,12 @@ import argparse
 import sys
 
 SAFE_HEADER= b'\xDE\xAD\xCA\xFE'
-def add_header(directory):
+def header_manipulation(directory, patch=None, rm=None):
     '''
-    '''
-    samples = glob.glob(directory)
-
-    for sample in samples:
-        try:
-            with open(sample, "rb") as fin:
-                real_data = fin.read()
-                if real_data[0:4] == b'\xde\xad\xca\xfe':
-                    print("[*] Safe header has already been appended. Exiting!")
-                    sys.exit(1)
-                new_binary = SAFE_HEADER + real_data
-                with open(sample, 'wb') as fout:
-                    fout.write(new_binary)
-                print("[+] Successfully patched %s with header" % (sample))
-        except IsADirectoryError:
-            print("[!] Error, specified directoy for got the '*'")
-            sys.exit(1)
-    return True
-
-
-def remove_header(directory):
-    '''
+    Name: header_manipulation
+    Purpose: Add a fake header to a binary to prevent accidental execution in 
+             the moving of binaries.
+    Return: Boolean value.
     '''
     samples = glob.glob(directory)
 
@@ -35,32 +17,59 @@ def remove_header(directory):
         try:
             with open(sample, "rb") as fin:
                 real_data = fin.read()
-                if real_data[0:4] == b'\xde\xad\xca\xfe':
-                    print("[+] Safe header identified. Removing.")
-                    real_data = real_data[4:]
-                    with open(sample, 'wb') as fout:
-                        fout.write(real_data)
+                if real_data[0:4] == b'\xde\xad\xca\xfe' and rm is None:
+                    print("[*] Safe header already exists within %s." % sample)
+                    return True
+
+                elif real_data[0:4] == b'\xde\xad\xca\xfe' and rm is not None:
+                    print("[*] Safe header identified in %s! Removing safe header!" % sample) 
+                    real_data = real_data[4:] # Remove four bytes
+                    try:
+                        with open(sample, 'wb') as fout:
+                            fout.write(real_data)
+                    except IOError as err:
+                        print("[!] Error: %s" % (err))
+                        return False
                     print("[+] Successfully restored %s." % (sample))
-                else:
-                    print("[+] ELF header already exists %s." % (sample))
+                    return True
 
+                elif real_data[0:4] != b'\xde\xad\xca\xfe' and rm is not None:
+                    print("[!] Magic header has already been removed")
+                    return True
+
+                elif real_data[0:4] != b'\xde\xad\xca\xfe' and patch is not None:
+                    new_binary = SAFE_HEADER + real_data
+                    try:
+                        with open(sample, 'wb') as fout:
+                            fout.write(new_binary)
+                    except IOError as err:
+                        print("[!] Error: %s" % (err))
+                        return False
+                    print("[+] Successfully patched %s with header %s" % (sample, SAFE_HEADER))
         except IsADirectoryError:
-            print("[!] Error, specified directoy for got the '*'")
-            sys.exit(1)
-    return True
+            print("[!] Error, specified directory for got the '*'")
+            return False
 
+    return True
 
 if __name__ == "__main__":
     print("[--<Project Safe Header>--]")
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dir", default="", help="Specify directory to add safety header to.",
-                        required=True)
-    parser.add_argument("-r", "--rm", default="", action="store_true", help="Specify directory to rmeove safety header to.",
-                        required=False)
+    parser.add_argument("-d", "--dir", default="",
+            help="Specify directory to add safety header to.", required=True)
+
+    parser.add_argument("-r", "--rm", default=None, action="store_true",
+            help="Specify directory to remove safety header to.", required=False)
+
+    parser.add_argument("-p", "--patch", default=None, action="store_true",
+            help="Specify to patch binary or not.", required=False)
     args = parser.parse_args()
 
-    if args.rm:
-        remove_header(args.dir)
+    if args.patch is not None and args.rm is not None:
+        print("[!!!] You cannot patch some binaries and remove the patch at the same time! [!!!]")
+        sys.exit(1)
 
-    elif add_header(args.dir):
-        print("[+] Successfully patched binaries with header")
+    if header_manipulation(args.dir, args.patch, args.rm):
+        print("[+] Successfully completed manipulation")
+    else:
+        print("[!] Something has gone horribly wrong with safeheader.")
