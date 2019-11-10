5a5946ba2153c5df934e00508aed28f1bed00571
Jared Stroud <1250113+jaredestroud@users.noreply.github.com>
2019-11-09 20:47:45 -0500

    Remove header CLI args

--- safeheader.py
+++ safeheader.py
@@ -25,12 +25,42 @@ def add_header(directory):
             sys.exit(1)
     return True
 
+
+def remove_header(directory):
+    '''
+    '''
+    samples = glob.glob(directory)
+
+    for sample in samples:
+        try:
+            with open(sample, "rb") as fin:
+                real_data = fin.read()
+                if real_data[0:4] == b'\xde\xad\xca\xfe':
+                    print("[+] Safe header identified. Removing.")
+                    real_data = real_data[4:]
+                    with open(sample, 'wb') as fout:
+                        fout.write(real_data)
+                    print("[+] Successfully restored %s." % (sample))
+                else:
+                    print("[+] ELF header already exists %s." % (sample))
+
+        except IsADirectoryError:
+            print("[!] Error, specified directoy for got the '*'")
+            sys.exit(1)
+    return True
+
+
 if __name__ == "__main__":
     print("[--<Project Safe Header>--]")
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--dir", default="", help="Specify directory to add safety header to.",
                         required=True)
+    parser.add_argument("-r", "--rm", default="", action="store_true", help="Specify directory to rmeove safety header to.",
+                        required=False)
     args = parser.parse_args()
 
-    if add_header(args.dir):
+    if args.rm:
+        remove_header(args.dir)
+
+    elif add_header(args.dir):
         print("[+] Successfully patched binaries with header")
